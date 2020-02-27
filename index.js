4ce7335d2af12cb6f8dfa9d1c3746b4223c1007f
Faiyaz <faiyaz@pop-os.localdomain>
2020-02-27 21:38:43 +0530

    add block 'browser back' button

--- index.js
+++ index.js
@@ -9,97 +9,94 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
 // Connecting to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB Atlas"))
-    .catch(error => console.log(error));
-
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log("Connected to MongoDB Atlas"))
+	.catch(error => console.log(error));
 
 // Importing Models
 const Question = require("./models/Question");
 
 // Declaring constants
-let question, questionList = [];
+let question,
+	questionList = [];
 let n = 5; // Number of questions
 let currentIndex;
 
-
 // Handling REQUESTS
 app.get("/", (req, res) => {
-    res.render("index");
+	res.render("index");
 });
 
 app.get("/start", (req, res) => {
-    currentIndex = 0;
-    Question.findRandom({}, {}, { limit: n }, (err, result) => {
-        if (err) {
-            console.log(error);
-        } else {
-            if (!result) {
-                console.log("Error!");
-            } else {
-                let i = 0;
-                questionList = [];
-                result.forEach(ques => {
-                    questionList.push({
-                        index: i,
-                        question: ques.question,
-                        A: ques.A,
-                        B: ques.B,
-                        C: ques.C,
-                        D: ques.D,
-                        answer: ques.answer,
-                        chosen: ""
-                    });
-                    i++;
-                });
-                res.redirect("/question");
-            }
-        }
-    });
+	currentIndex = 0;
+	Question.findRandom({}, {}, { limit: n }, (err, result) => {
+		if (err) {
+			console.log(error);
+		} else {
+			if (!result) {
+				console.log("Error!");
+			} else {
+				let i = 0;
+				questionList = [];
+				result.forEach(ques => {
+					questionList.push({
+						index: i,
+						question: ques.question,
+						A: ques.A,
+						B: ques.B,
+						C: ques.C,
+						D: ques.D,
+						answer: ques.answer,
+						chosen: ""
+					});
+					i++;
+				});
+				res.redirect("/question");
+			}
+		}
+	});
 });
 
 app.get("/question", (req, res) => {
-    question = { ...questionList[currentIndex] }
-    res.render("question", { question });
+	question = { ...questionList[currentIndex] };
+	res.render("question", { question });
 });
 
 app.post("/question", (req, res) => {
-    let option = req.body.option;
-    let move = req.body.move;
-    if (typeof option != "undefined") {
-        questionList[currentIndex].chosen = option;
-    }
-    if (move === "Prev") {
-        if (currentIndex > 0)
-            currentIndex--;
-    } else if (move === "Next") {
-        if (currentIndex < n - 1)
-            currentIndex++;
-    }
-    res.redirect("/question");
+	let option = req.body.option;
+	let move = req.body.move;
+	if (typeof option != "undefined") {
+		questionList[currentIndex].chosen = option;
+	}
+	if (move === "Prev") {
+		if (currentIndex > 0) currentIndex--;
+	} else if (move === "Next") {
+		if (currentIndex < n - 1) currentIndex++;
+	}
+	res.redirect("/question");
 });
 
 app.get("/submit", (req, res) => {
-    let score = 0;
-    let msg;
-    questionList.forEach(question => {
-        if (question.chosen === question.answer) {
-            score++;
-        }
-    });
-    if (score > 4)
-        msg = "You are awesome! :P";
-    else if (score > 2)
-        msg = "Nice try! :)"
-    else
-        msg = "Better luck next time! ;(";
-    res.render("score", { score, msg });
+	let score = 0;
+	let msg;
+	questionList.forEach(question => {
+		if (question.chosen === question.answer) {
+			score++;
+		}
+	});
+	if (score > 4) msg = "You are awesome! :P";
+	else if (score > 2) msg = "Nice try! :)";
+	else msg = "Better luck next time! ;(";
+	res.render("score", { score, msg });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
